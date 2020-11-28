d47572bff60dfcac8a6c2b11d99e3c719c35f53e
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2020-11-28 16:48:30 -0500

    Update ComputeMethod.cpp

--- src/ComputeMethod.cpp
+++ src/ComputeMethod.cpp
@@ -5,19 +5,28 @@ using namespace ComputeMethods;
 
 // -------------------------------  Direct  -------------------------------
 void Direct::computeAccelerations(vector<body>& bodies) {
-	const double G = 6.67408 * pow(10, -11); // 1.184 * pow(10, -4);
+	const double G = 1.184 * pow(10, -4);// 6.67408 * powf(10, -11);
 	int N = bodies.size();
+	for (int i = 0; i < N; i++) {
+		bodies[i].ax = 0;
+		bodies[i].ay = 0;
+	}
+
 	for (int i = 0; i < N; i++) {
 		body& bi = bodies[i];
 		for (int j = i + 1; j < N; j++) {
 			body& bj = bodies[j];
-
+	
 			double dx = bj.x - bi.x;
 			double dy = bj.y - bi.y;
-			double r = sqrtf(powf(dx, 2) + powf(dy, 2));
-			double ki = G * bj.mass / powf(r, 3);
-			double kj = G * bi.mass / powf(r, 3);
-
+			double r = sqrt(dx * dx + dy * dy); // sqrt(pow(dx, 2) + pow(dy, 2)); // 
+			//if (r < 1E-3)
+			//	continue;
+			
+			double r3 = r * r * r; // pow(r, 3); // 
+			double ki = G * bj.mass / r3;
+			double kj = G * bi.mass / r3;
+			
 			bi.ax += ki * dx;
 			bi.ay += ki * dy;
 			bj.ax -= kj * dx;
@@ -26,7 +35,5 @@ void Direct::computeAccelerations(vector<body>& bodies) {
 	}
 }
 
+
 // -------------------------------  Direct Multi Thread  -------------------------------
-void DirectMultiThread::computeAccelerations(vector<body>& bodies) {
-	throw "Not implemented";
-}
