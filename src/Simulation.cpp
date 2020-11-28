20103e7ccdde209044a9a5452478ab3b1aa0b61b
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2020-11-28 16:49:19 -0500

    Update Simulation.cpp

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -5,25 +5,48 @@ using namespace SystemInitializers;
 using namespace ComputeMethods;
 using namespace TimeSteps;
 
-Simulation::Simulation(IntegrationMethod* iMethod, ComputeMethod* cMethod): integrator(iMethod), solver(cMethod) {}
+Simulation::Simulation(IntegrationMethod* iMethod, ComputeMethod* cMethod, TimeSteps::TimeStep* timeSchedule): 
+	integrator(iMethod), solver(cMethod), timeStep(timeSchedule) {}
 
-void Simulation::addSystem(SystemInitializer& initializer) {
-	int N = initializer.getN();
-	for (int i = 0; i < N; i++)
-		bodies.push_back(body());
-	initializer.init(bodies);
+
+void Simulation::start() {
+	integrator->init(bodies, solver);
+	solver->init(bodies);
+	inProgress = true;
 }
 
-void Simulation::update(TimeStep& dt) { // move TimeStep to constructor or send as a reference
-	for (body& i : bodies) {
-		i.ax = 0;
-		i.ay = 0;
+void Simulation::end() {
+	inProgress = false;
+	solver->close();
+}
+
+void Simulation::addSystem(SystemInitializer* initializer) {
+	int N = initializer->getN();
+	for (int i = 0; i < N; i++)
+		bodies.push_back(body());
+	initializer->init(bodies, bodies.size() - N, bodies.size());
+}
+
+
+double Simulation::getTimeStep() {
+	return timeStep->getTimeStep();
+}
+
+void Simulation::computeAccelerations() {
+	solver->computeAccelerations(bodies);
+}
+
+void Simulation::update() {
+	if (!inProgress) {
+		std::cout << "Start simulation before updating \n";
+		throw; // make a better method
 	}
-	integrator->integrate(bodies, dt, solver);
+
+	integrator->integrate(bodies, timeStep, solver);
 }
 
 double Simulation::getEnergy() {
-	const double G = 6.67408 * pow(10.f, -11.f); // 1.184 * pow(10, -4);
+	const double G = 1.184 * pow(10, -4); // 6.67408 * pow(10.f, -11.f); // 1.184 * pow(10, -4);
 	double E = 0, Ek = 0, Ep = 0;
 	for (int i = 0; i < bodies.size(); i++) {
 		body& bi = bodies[i];
@@ -36,7 +59,7 @@ double Simulation::getEnergy() {
 			if (j == i)
 				continue;
 			body& bj = bodies[j];
-			double r = sqrt(pow(bj.x - (double)bi.x, 2) + pow(bj.y - (double)bi.y, 2));
+			double r = sqrt(pow(bj.x - bi.x, 2) + pow(bj.y - bi.y, 2));
 			Ep += -G * bi.mass * bj.mass / 2.0f / r;
 		}
 	}
